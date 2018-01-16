4c48e53ac7fa2db0c44a9ef7650290029625dae2
測試一號 <abcabc>
2018-01-16 16:41:34 +0800

    0116

--- app/src/main/java/forfun/good/a2018011503/MainActivity.java
+++ app/src/main/java/forfun/good/a2018011503/MainActivity.java
@@ -1,6 +1,8 @@
 package forfun.good.a2018011503;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText ed;
-    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sp = getSharedPreferences("mydata",MODE_PRIVATE);
         String str = sp.getString("地名","");
         TextView tv = findViewById(R.id.textView);
+        tv.setText(str);
+    }
+    public void clickset(View v)
+    {
+        Intent it = new Intent(MainActivity.this,SettingsActivity.class);
+        startActivity(it);
+    }
+    public void clickread(View v)
+    {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String str = sp.getString("mydata", "");
+        TextView tv = (TextView) findViewById(R.id.textView2);
         tv.setText(str);
     }
 }
